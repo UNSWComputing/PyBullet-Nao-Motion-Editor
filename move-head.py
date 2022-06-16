f5922809fd04d833dc2b3dc6eb3456518c524e86
n33r4j <52103565+n33r4j@users.noreply.github.com>
2022-06-17 03:08:09 +1000

    Add button to capture keyframes and write to .pos file at end

--- move-head.py
+++ move-head.py
@@ -10,15 +10,16 @@ from UtilityFuncs import Lerp
 
 if __name__ == "__main__":
     simulation_manager = SimulationManager()
-
     client = simulation_manager.launchSimulation(gui=True, auto_step=False)
+    
+    simulation_manager.setGravity(client, [0.0, 0.0, 0.0])
     
     robot = simulation_manager.spawnNao(client, spawn_ground_plane=True)
 
     time.sleep(1.0)
     joint_parameters = list()
 
-    robot.goToPosture()
+    # robot.goToPosture()
     # for name, joint in robot.joint_dict.items():
     #     print(f"{name:14} | {joint.getLowerLimit():12} | {joint.getUpperLimit():12} | {robot.getAnglesPosition(name):6}")
 
@@ -26,27 +27,22 @@ if __name__ == "__main__":
         yaw_angle = 0.0
         upperlimit = robot.joint_dict.get("HeadYaw").getUpperLimit()
         lowerlimit = robot.joint_dict.get("HeadYaw").getLowerLimit()
-        direction = 1 # 1, -1 -> Left, Right
-        curr_angle = robot.getAnglesPosition("HeadYaw")
+        direction = 1 # 1, -1 -> L, R
         step = 0.2
         delay = 0.5
         prevTime = 0
+        firstTime = True
+        speed = 0.1
         while True:
-            currTime = time.time()
-            if currTime > prevTime + delay:
-                prevTime = currTime
-                if direction > 0:
-                    if curr_angle+step < upperlimit: # Left
-                        curr_angle += step
-                    else:
-                        direction = -1
-                elif direction < 0:
-                    if curr_angle-step > lowerlimit: # Right
-                        curr_angle -= step
-                    else:
-                        direction = 1
-                robot.setAngles("HeadYaw",curr_angle, 1.0)
-            simulation_manager.stepSimulation(client)
+            if firstTime:
+                firstTime = False
+                robot.setAngles("HeadYaw", lowerlimit, speed)
+            curr_angle = robot.getAnglesPosition("HeadYaw")
+            if curr_angle >= upperlimit-0.05:
+                robot.setAngles("HeadYaw", lowerlimit, speed)
+            elif curr_angle <= lowerlimit+0.05:
+                robot.setAngles("HeadYaw", upperlimit, speed)
+            # simulation_manager.stepSimulation(client)
 
     except KeyboardInterrupt:
         pass
