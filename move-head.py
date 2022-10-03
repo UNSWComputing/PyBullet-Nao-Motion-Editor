73f07bd35378162a97f186b1dda376ced43d1285
n33r4j <52103565+n33r4j@users.noreply.github.com>
2022-10-03 15:13:40 +1100

    Add goalposts and ball

--- move-head.py
+++ move-head.py
@@ -3,34 +3,77 @@
 from datetime import timedelta
 import sys, time
 import pybullet as pb
+import pybullet_data as pd
 from qibullet import SimulationManager
 from qibullet import NaoVirtual
 # from UtilityFuncs import Lerp
 from MotionHandle import MotionHandle as MH
 from MotionPlayer import MotionPlayer as MP
 
+PI = 3.1415926535
 
 if __name__ == "__main__":
     simulation_manager = SimulationManager()
     client = simulation_manager.launchSimulation(gui=True, auto_step=False)
-    
+    pb.setAdditionalSearchPath(pd.getDataPath())
     pb.resetDebugVisualizerCamera( cameraDistance=0.5, cameraYaw=65, cameraPitch=-25, cameraTargetPosition=[0,0,0.35])
+    
+    # Creates better meshes for goalposts. Uses Separating Axis Theorem(SAT) based convex
+    # collision detection (instead of GJK and EPA)
+    pb.setPhysicsEngineParameter(enableSAT=1)
+    
     simulation_manager.setGravity(client, [0.0, 0.0, -9.81])
     
+    pb.loadURDF("models/spl-22-field.urdf", 
+                basePosition=[0,0,0],
+                baseOrientation=pb.getQuaternionFromEuler([PI/2,0,0]),
+                useFixedBase=1,
+                flags=pb.URDF_USE_MATERIAL_COLORS_FROM_MTL, 
+                physicsClientId=client)
+
+    # Currently, the goalpost uses a convex mesh, i.e. objects can't pass the goal line.
+    # Might need to break the goalpost into 2 or more convex parts and combine them in urdf.
+    pb.loadURDF("models/spl-goalpost.urdf", 
+                basePosition=[-4.5,0,0.04],
+                baseOrientation=pb.getQuaternionFromEuler([PI/2,0,0]),
+                useFixedBase=1,
+                flags=pb.URDF_USE_MATERIAL_COLORS_FROM_MTL,
+                physicsClientId=client)
+
+    pb.loadURDF("models/spl-goalpost.urdf", 
+                basePosition=[4.5,0,0.04],
+                baseOrientation=pb.getQuaternionFromEuler([PI/2,0,PI]),
+                useFixedBase=1,
+                flags=pb.URDF_USE_MATERIAL_COLORS_FROM_MTL | pb.URDF_INITIALIZE_SAT_FEATURES, 
+                physicsClientId=client)
+
+    # This is from pybullet
+    ball = pb.loadURDF("soccerball.urdf",[0,0,1], globalScaling=0.1)
+    pb.changeDynamics(ball,-1,linearDamping=0, angularDamping=0, rollingFriction=0.001, spinningFriction=0.001)
+    pb.changeVisualShape(ball,-1,rgbaColor=[0.8,0.8,0.8,1])
+
+    # This was made by me
+    # pb.loadURDF("models/spl-22-ball.urdf",
+    #             basePosition=[0.3,0,0.3],
+    #             baseOrientation=pb.getQuaternionFromEuler([0,0,0]),
+    #             flags=pb.URDF_USE_MATERIAL_COLORS_FROM_MTL, 
+    #             physicsClientId=client)
+
     robot = simulation_manager.spawnNao(client, spawn_ground_plane=True)
 
-    pb.changeDynamics(robot.robot_model, -1, mass=0) # Sets the mass of the base link(-1) to 0.
+    # pb.changeDynamics(robot.robot_model, -1, mass=0.000000001) # Sets the mass of the base link(-1) to 0.
     
-    pb.resetBasePositionAndOrientation(robot.robot_model, [0.0, 0.0, 0.5], pb.getQuaternionFromEuler([0.0, 0.0, 0.0]))
+    pb.resetBasePositionAndOrientation(robot.robot_model, [0.0, 0.0, 0.53], pb.getQuaternionFromEuler([0.0, 0.0, 0.0]))
 
     time.sleep(1.0)
     joint_parameters = list()
 
     motion_handle = MH()
-    # motion_handle.readPosFile("sit.pos")
-    motion_handle.readPosFile("getupFront.pos")
-    # motion_handle.readPosFile("ukemiBack.pos")
-    # motion_handle.readPosFile("sample_motion-1.pos")
+    # motion_handle.readPosFile("pos/sit.pos")
+    # motion_handle.readPosFile("pos/getupFront.pos")
+    # motion_handle.readPosFile("pos/getupBack.pos")
+    motion_handle.readPosFile("pos/ukemiBack.pos")
+    # motion_handle.readPosFile("pos/sample_motion-1.pos")
     motion_player = MP(robot, motion_handle)
     # MP.loop = True
 
