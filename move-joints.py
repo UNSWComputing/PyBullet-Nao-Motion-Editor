81b7ccee21004e2ec7de953254cc669daec4a180
n33r4j <52103565+n33r4j@users.noreply.github.com>
2023-01-26 00:51:49 +1100

    Add outline for reset and joint mirroring buttons

--- move-joints.py
+++ move-joints.py
@@ -40,21 +40,50 @@ if __name__ == "__main__":
     rotate_Y_slider = pb.addUserDebugParameter("  Rotate Y", -PI, PI, 0.0)
     rotate_Z_slider = pb.addUserDebugParameter("  Rotate Z", -PI, PI, 0.0)
 
+    # Not ideal but there doesn't seem to be a better way for now.
     duration_slider = pb.addUserDebugParameter("  Duration(ms)", 50, 5000, 1000)
+
+    mirror_joints_button = pb.addUserDebugParameter("  Mirror Joints", 1, 0, 0)
+    reset_joints_button = pb.addUserDebugParameter("  Reset Joints", 1, 0, 0)
 
     capture_joint_values_button = pb.addUserDebugParameter("  Capture Keyframe", 1, 0, 0)
 
     try:
         motion_1 = MH()
         button_counter = 1
+        reset_joints_button_counter = 1
         while True:
             joint_vals = []
-            for joint_parameter in joint_parameters:
-                val = pb.readUserDebugParameter(joint_parameter[0])
-                joint_vals.append(val)
-                robot.setAngles(
-                    joint_parameter[1],
-                    val, 1.0)
+            should_mirror_joints = pb.readUserDebugParameter(mirror_joints_button)
+
+            reset_joints = pb.readUserDebugParameter(reset_joints_button)
+            # print(reset_joints, reset_joints_button_counter)
+
+            if reset_joints >= reset_joints_button_counter:
+                # Can't set slider positions programatically. So this doesn't quite work yet.
+                for joint_parameter in joint_parameters:
+                    pb.writeUserDebugParameter(joint_parameter[0], 0.0)
+                    robot.setAngles(
+                        joint_parameter[1],
+                        0.0, 1.0)
+                reset_joints_button_counter += 1
+            else:
+                for joint_parameter in joint_parameters:
+                    val = pb.readUserDebugParameter(joint_parameter[0])
+                    joint_vals.append(val)
+                    
+                    if should_mirror_joints % 2 == 1 and joint_parameter[1][0] != 'H':
+                        # mirror joints
+                        # Joint vals aren't always just negative. Corrections need to be
+                        # made for each pair.
+                        side = 'L' if joint_parameter[1][0] == 'R' else 'R'
+                        robot.setAngles(
+                            [joint_parameter[1], (side + joint_parameter[1][1:])],
+                            [val, -val], 1.0)
+                    else:
+                        robot.setAngles(
+                            joint_parameter[1],
+                            val, 1.0)
 
             X_rot_val = pb.readUserDebugParameter(rotate_X_slider)
             Y_rot_val = pb.readUserDebugParameter(rotate_Y_slider)
